7f82fbfa0abbe13878e6d91b6bf29f97e9fe6a54
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2022-10-19 22:57:02 -0600

    ya mejor

--- Busqueda binaria/BusquedaBin.cpp
+++ Busqueda binaria/BusquedaBin.cpp
@@ -2,21 +2,31 @@
 #include <conio.h>
 
 #define GetSize(array_enteros) (sizeof(array_enteros)/sizeof(*(array_enteros)))
-
+int busquedaBin(int num[], int clave);
 
 int main()
 {
     int nums[] = {10, 18, 23, 29, 32, 40, 47, 51, 63};
-    int clave,bajo,alto,mitad,valorMitad;
+    int clave{};
    
     
 
     std::cout << "Ingrese numero a buscar: ";
     std::cin >> clave;
     //Algoritmo para la busqueda
-   
     
+    if (busquedaBin(nums,clave) == -1 )
+    {
+        std::cout << "No se encontro el numero dentro del arreglo";
+    }
+    else
+    {
+        std::cout << "Se encontro el numero: " << clave << " en la posicion: " << busquedaBin(nums,clave);
+    }
     
+
+    
+    #if 0
     bajo = 0;
     alto = GetSize(nums);
     while (bajo <= alto)
@@ -40,6 +50,34 @@ int main()
             mitad = ((bajo + alto)/2);
         }
     }   
-    
+    #endif 
     return 0;
 }
+
+int busquedaBin(int num[], int clave)
+{
+    int central, bajo, alto;
+    int valorCentral;
+    bajo = 0;
+    alto = sizeof(num);
+    while (bajo <= alto)
+    {
+        central = (bajo + alto)/2;
+        valorCentral = num[central];
+        if (clave == valorCentral)
+        {
+
+            return central;
+        }
+        else if (clave < valorCentral)
+        {
+            alto = central -1;
+        }
+        else
+        {
+            bajo = central + 1;
+        }
+    }
+    return -1;
+}
+
