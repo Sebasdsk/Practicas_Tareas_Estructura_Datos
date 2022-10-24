c8d3c948b7ecc345f67d9a994637ee16baebaf04
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2022-10-23 23:32:01 -0600

    23/10/22

--- Busqueda binaria/BusquedaBin.cpp
+++ Busqueda binaria/BusquedaBin.cpp
@@ -9,7 +9,6 @@ void busquedaSec(int num[], int clave);
 
 int main()
 {
-    
     int nums[200] = {};
     int clave{}, opc;
 
@@ -19,18 +18,20 @@ int main()
     }
     ordenar(nums);                      // Funcion que ordena los elementos dentro del arreglo
     
+    
     std::cout << "Bienvenido al programa: \n";
     do{ 
     std::cout << "Que desea realizar? \n";
-    std::cout << "1.- Busqueda lineal: \n 2.- Busqueda binaria: \n";
+    std::cout << "1.- Busqueda Binaria: \n2.- Busqueda Lineal: \n";
     std::cin >> opc;
 
-        if (opc == 1)
+        
+        switch (opc)
         {
+        case 1:
             std::cout << "Ingrese numero a buscar: ";
             std::cin >> clave;
             //Algoritmo para la busqueda
-
             if (busquedaBin(nums,clave) == -1 )
             {
                 std::cout << "No se encontro el numero dentro del arreglo";
@@ -39,19 +40,18 @@ int main()
             {
                 std::cout << "Se encontro el numero: " << clave << " en la posicion: " << busquedaBin(nums,clave);
             }
-        }
-        else if (opc == 2)
-        {
+            break;
+        case 2:
             std::cout << "Ingrese numero a buscar: ";
             std::cin >> clave;
             busquedaSec(nums,clave);
+            break;
+        
+        default:
+         std::cout << "Ingrese una opcion valida: \n";
+            break;
         }
-        else
-        {
-        std::cout << "Ingrese una opcion valida:";
-        }
-    }
-    while (opc != 1 && opc != 2);
+    }while ((opc != 1) && (opc != 2));
     
  
     return 0;
@@ -62,7 +62,7 @@ int busquedaBin(int num[], int clave)
     int central, bajo, alto;
     int valorCentral;
     bajo = 0;
-    alto = sizeof(num);
+    alto = 200 - 1;
     while (bajo <= alto)
     {
         central = (bajo + alto)/2;
