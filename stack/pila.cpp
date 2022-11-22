452bb16f09e9d8ec0d1489c9997f6f3c60673317
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2022-11-22 16:13:41 -0700

    Pila bn, creo

--- stack/pila.cpp
+++ stack/pila.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 
 
+
 const int tam_pila = 5;  //declaramos una constante que es el tamaño de la pila
 
-struct stack
-{
-    int tope;
-    int elemento[tam_pila];
-};
+    int tope; //tope es la posicion que se va tomar en el vector de la pila
+    int elemento[tam_pila];  //Declaramos el vector de la pila con el tamaño constante de tam_pila = 5
 
-stack pila;
+void mostrar();
+void restante();
 
 void menu()
 {
     std::cout << "\n Que desea hacer? \n";
-    std::cout << "1.Mostrar Pila \n2.Agregar Elementos \n3.Quitar Elementos \n4.Salir \n";
+    std::cout << "1.Mostrar Pila \n2.Agregar Elementos \n3.Quitar Elementos \n4.Vaciar la pila \n5.Salir \n";
 }
 
-void iniciar()       //Funcion que inicia la pila
+void iniciar()       //Funcion que inicia la pila y que a su vez vacia la pila
 {
-    pila.tope = -1;
+    tope = -1;
 
     for(int i = 0; i < tam_pila; i++)
     {
-        pila.elemento[i] = 0;
+        elemento[i] = 0;
     }
+    mostrar();
 }
 
 bool vacio()
 {
-    return pila.tope == -1; //regresa una comparacion que puede ser verdadera o falsa
+    return tope == -1; //regresa una comparacion que puede ser verdadera o falsa
 }
 
 bool lleno()
 {
-    return pila.tope == tam_pila -1; //
+    return tope == tam_pila -1; //regresa verdadero si la comparacion que si el tope es igual al tamaño de la pila - 1 
+                                //el maximo del tope es 4 por como funcionan los vectores en c++ que se cuenta del 0 restando 1 al final del tamaño que vemos en codigo
 }
 
 void agregar(int x)
@@ -45,9 +46,10 @@ void agregar(int x)
     }
     else
     {
-        pila.tope++;
-        pila.elemento[pila.tope] = x;
+        tope++;
+        elemento[tope] = x;
         std::cout << "Elemento " << x << " Agregado exitosamente \n";
+        mostrar();
     }
 }
 
@@ -59,9 +61,11 @@ void quitar()
     }
     else
     {
-        int tmp = pila.elemento[pila.tope];
-        pila.elemento[pila.tope] = 0;
-        pila.tope--;
+        int tmp = elemento[tope];
+        elemento[tope] = 0;
+        tope--;
+        std::cout << "El elemento ha sido removido exitosamente \n";
+        mostrar();
     }
 }
 
@@ -70,33 +74,41 @@ void mostrar()
     if (vacio())
     {
         std::cout << "Pila Vacia: ";
+        restante();
     }
     else if (lleno())
     {
         std::cout << "La pila esta llena \nEstos son los datos: \n";
-        for(int i = 0; i <= pila.tope; i++)
+        for(int i = 0; i <= tope; i++)
         {
-            std::cout << "Espacio ["<<i<<"]: " <<pila.elemento[i] << std::endl;
+            std::cout << "Espacio ["<<i<<"]: " <<elemento[i] << std::endl;
         }
+        
     }
     else
     {
-        for(int i = 0; i <= pila.tope; i++)
+        for(int i = 0; i <= tope; i++)
         {
-            std::cout << "Espacio ["<<i<<"]: " <<pila.elemento[i] << std::endl;
+            std::cout << "Espacio ["<<i<<"]: " <<elemento[i] << std::endl;
         }
+        restante();
     }
 }
 
+void restante()
+{
+    std::cout <<"Quedan  " <<  tam_pila - (tope + 1) << " Espacios disponibles " << std::endl;
+}
 
 
 int main()
 {
     iniciar();
-    int elemento, opc{0};
+    int usrElemento, opc{0};
     std::cout << "Bienvenido: ";
     do
     {
+        std::cout << "\n";
         menu();
         std::cin >> opc;
         switch (opc)
@@ -104,25 +116,31 @@ int main()
         case 1:
             mostrar();
             
+            
             break;
         case 2:
             std::cout << "Numero que desea ingresar: ";
-            std::cin >> elemento;
-            agregar(elemento);
-
+            std::cin >> usrElemento;
+            agregar(usrElemento);
+            
             break;
-
         case 3:
             quitar();
+            
             break;
             
         case 4: 
+            iniciar();
+            std::cout << "La pila se ha vaciado exitosamente \n";
+            
+            break;
+        case 5:
             return 0;
         default:
             std::cout << "Ingrese una opcion valida: ";
             break;
         }
-    }while (opc != 4);
+    }while (opc != 5);
     system("PAUSE");
     return 0; 
 
